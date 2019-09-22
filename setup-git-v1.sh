f52b659107a92f4ad868521151b1230607ec153a
marcusnow7 <marcusnow7@gmail.com>
2019-09-22 09:57:10 +0100

    first commit setup

---
+++ setup-git-v1.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+# change into your name and email.
+
+git init
+git config --global user.name "marcusnow7"
+git config --global user.email "marcusnow7@gmail.com"
+sudo git config --system core.editor nano
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=25000'
+git config --global push.default simple
+
+
+echo "################################################################"
+echo "##################    SET UP Complete      #####################"
+echo "################################################################"
